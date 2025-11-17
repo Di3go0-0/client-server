1c669ce7140775597f93972b5b10c68a0be1c744
Di3go0-0 <d.rincon@utp.edu.co>
2025-11-17 13:34:35 -0500

    update rooms.gateway

--- final-project/api/src/websockets/gateways/rooms.gateway.ts
+++ final-project/api/src/websockets/gateways/rooms.gateway.ts
@@ -17,6 +17,17 @@ export class RoomsGateway {
     private readonly usersConnected: UsersService,
   ) { }
 
+  // Obtener usuarios activos y no activos de una room
+  private async getFormattedUsersInRoom(id: number) {
+    const userDetails = await this.roomsService.GetUsersDetailInRoom(id);
+    return userDetails.map(user => ({
+      id: user.user_id,
+      userName: user.username,
+      email: user.email,
+      online: !!this.usersConnected.getUserById(user.user_id),
+    }));
+  }
+
   // Obtener todas las Rooms
   @SubscribeMessage('get_rooms')
   async handleGetRooms() {
@@ -40,7 +51,7 @@ export class RoomsGateway {
   // Usuario entra a una room
   @SubscribeMessage('join_room')
   async handleJoinRoom(
-    @MessageBody() { roomId }: { roomId: number },
+    @MessageBody() roomId: number,
     @ConnectedSocket() client: Socket
   ) {
     const user = this.usersConnected.getUser(client.id)
@@ -51,8 +62,7 @@ export class RoomsGateway {
     const usersActivesInRoom = `users_active_room_id_${roomId}`;
     client.join(usersActivesInRoom);
 
-    const ids = await this.roomsService.GetUsersIdInRoom(roomId);
-    const users = ids.map(id => this.usersConnected.getUserById(id));
+    const users = await this.getFormattedUsersInRoom(roomId);
 
     // Notificar a todos los suscriptores de la sala
     this.server.to(usersActivesInRoom).emit('users.room.updated', users);
@@ -61,7 +71,7 @@ export class RoomsGateway {
   // Usuario sale de una room
   @SubscribeMessage('left_room')
   async handleLeftRoom(
-    @MessageBody() { roomId }: { roomId: number },
+    @MessageBody() roomId: number,
     @ConnectedSocket() client: Socket
   ) {
     const user = this.usersConnected.getUser(client.id)
@@ -72,8 +82,7 @@ export class RoomsGateway {
     const usersActivesInRoom = `users_active_room_id_${roomId}`;
     client.leave(usersActivesInRoom);
 
-    const ids = await this.roomsService.GetUsersIdInRoom(roomId);
-    const users = ids.map(id => this.usersConnected.getUserById(id));
+    const users = await this.getFormattedUsersInRoom(roomId);
 
     this.server.to(usersActivesInRoom).emit('users.room.updated', users);
   }
@@ -86,8 +95,7 @@ export class RoomsGateway {
   ) {
     const usersActivesInRoom = `users_active_room_id_${id}`;
     client.join(usersActivesInRoom);
-    const ids = await this.roomsService.GetUsersIdInRoom(id);
-    const users = ids.map(id => this.usersConnected.getUserById(id));
+    const users = await this.getFormattedUsersInRoom(id);
 
     this.server.to(client.id).emit('users.room.updated', users);
   }
@@ -99,8 +107,7 @@ export class RoomsGateway {
   ) {
     const roomIdUsers = `users_active_room_id_${id}`;
 
-    const ids = await this.roomsService.GetUsersIdInRoom(id);
-    const users = ids.map(id => this.usersConnected.getUserById(id));
+    const users = await this.getFormattedUsersInRoom(id);
 
     // Mandas la nueva lista a TODOS los subscriptores
     this.server.to(roomIdUsers).emit('users.room.updated', users);
